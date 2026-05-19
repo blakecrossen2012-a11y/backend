97f0e5edcd8aecf2f907fb8a8937c7d764f1ec5e
blakecrossen2012-a11y <blakecrossen2012@gmail.com>
2026-05-19 18:02:01 -0400

    Refactor server.js for improved structure and clarity

--- server.js
+++ server.js
@@ -6,38 +6,41 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
+    cors: {
+        origin: "*"
+    }
 });
 
-const players = {};
+app.use(express.static("public"));
+
+let players = {};
 
 io.on("connection", (socket) => {
 
-  players[socket.id] = {
-    x: 200,
-    y: 200
-  };
-
-  io.emit("players", players);
-
-  socket.on("move", (data) => {
-    if (!players[socket.id]) return;
-
-    players[socket.id].x = data.x;
-    players[socket.id].y = data.y;
+    players[socket.id] = {
+        x: 200,
+        y: 200
+    };
 
     io.emit("players", players);
-  });
 
-  socket.on("disconnect", () => {
-    delete players[socket.id];
-    io.emit("players", players);
-  });
+    socket.on("move", (data) => {
+        if (!players[socket.id]) return;
 
+        players[socket.id].x = data.x;
+        players[socket.id].y = data.y;
+
+        io.emit("players", players);
+    });
+
+    socket.on("disconnect", () => {
+        delete players[socket.id];
+        io.emit("players", players);
+    });
 });
 
-server.listen(3000, () => {
-  console.log("running");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Running on", PORT);
 });
